495e317696aec3b7c3509d36012640fcc99022db
mcewball13 <mcewen1984@hotmail.com>
2022-06-17 15:03:27 -0700

    added soved mini and solved 1-4 oop

--- 09-NodeJS/01-Activities/20-Stu_Inquirer-Users/Solved/index.js
+++ 09-NodeJS/01-Activities/20-Stu_Inquirer-Users/Solved/index.js
@@ -24,7 +24,33 @@ inquirer
   .then((data) => {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile(filename, generateHTML(data), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
+
+
+  function generateHTML({name, stack, contact}) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${stack}</p>
+        <p>${contact}</p>
+    </div>
+</div>
+</div>
+</body>
+</html>
+    `
+  }
